6723a1577dfa543651b609fb64bbd06ea34829e0
eoberortner <eoberortner@gmail.com>
2015-02-23 21:16:12 -0800

    added BSD-3 Clause License statement

--- src/main/java/org/cidarlab/eugene/data/genbank/GenbankImporter.java
+++ src/main/java/org/cidarlab/eugene/data/genbank/GenbankImporter.java
@@ -1,63 +1,58 @@
-/*
-Copyright (c) 2012 Boston University.
-All rights reserved.
-Permission is hereby granted, without written agreement and without
-license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
-
-IN NO EVENT SHALL BOSTON UNIVERSITY BE LIABLE TO ANY PARTY
-FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-BOSTON UNIVERSITY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-BOSTON UNIVERSITY SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND BOSTON UNIVERSITY HAS
-NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-ENHANCEMENTS, OR MODIFICATIONS.
+/* Copyright (c) 2015, Boston University
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list 
+ * of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be 
+ * used to endorse or promote products derived from this software without specific prior 
+ * written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
  */
+
 
 package org.cidarlab.eugene.data.genbank;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.biojava.bio.seq.Feature;
-import org.biojava.bio.seq.Sequence;
-import org.biojava.bio.seq.SequenceIterator;
-import org.biojava.bio.seq.io.SeqIOTools;
 import org.biojava3.core.sequence.DNASequence;
-import org.biojava3.core.sequence.ProteinSequence;
-import org.biojava3.core.sequence.compound.AminoAcidCompound;
-import org.biojava3.core.sequence.compound.AminoAcidCompoundSet;
-import org.biojava3.core.sequence.compound.DNACompoundSet;
 import org.biojava3.core.sequence.compound.NucleotideCompound;
 import org.biojava3.core.sequence.features.FeatureInterface;
 import org.biojava3.core.sequence.io.GenbankReaderHelper;
 import org.biojava3.core.sequence.template.AbstractSequence;
-import org.cidarlab.eugene.Eugene;
-import org.cidarlab.eugene.constants.EugeneConstants;
 import org.cidarlab.eugene.dom.Device;
 import org.cidarlab.eugene.dom.NamedElement;
-import org.cidarlab.eugene.dom.PropertyValue;
-import org.cidarlab.eugene.dom.Part;
 import org.cidarlab.eugene.dom.PartType;
 import org.cidarlab.eugene.exception.EugeneException;
 import org.cidarlab.eugene.interp.SymbolTable;
 
-
+/**
+ * 
+ * @author Ernst Oberortner
+ */
 public class GenbankImporter {
 
 	private SymbolTable symbols;
@@ -66,31 +61,6 @@ public class GenbankImporter {
 		this.symbols = symbols;
 	}
 	
-//	public Part importPart(PartType objPartType, String sPartName)
-//			throws Exception {
-//
-//		// load the genbank file
-//		String sGenBank = getGenBank(sPartName);
-//
-//		// convert the string to
-//		InputStream is = new ByteArrayInputStream(sGenBank.getBytes());
-//
-//		// now, use BioJava's feature to read the GenBank data
-//		String sPartSeq = readGenBank(new BufferedReader(new InputStreamReader(
-//				is)));
-//
-//		Part objPart = new Part(objPartType, sPartName);
-//
-//		PropertyValue pv = new PropertyValue(
-//				objPart.getProperty(EugeneConstants.SEQUENCE_PROPERTY));
-//		pv.setTxt(sPartSeq);
-//		objPart.setPropertyValue(
-//				objPart.getProperty(EugeneConstants.SEQUENCE_PROPERTY), 
-//				pv);
-//
-//		return objPart;
-//	}
-
 	private String getGenBank(String sPartName) throws Exception {
 		URL url = new URL("http://cambridgeigem.org/gbdownload/BBa_"
 				+ sPartName + ".gb");
