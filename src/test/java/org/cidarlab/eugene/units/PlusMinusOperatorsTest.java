174923a8a33daf80a51c8a18bb346a8b78d9fc7f
eoberortner <eoberortner@gmail.com>
2015-05-25 12:27:12 -0700

    unit-testing the + operator

--- src/test/java/org/cidarlab/eugene/units/PlusMinusOperatorsTest.java
+++ src/test/java/org/cidarlab/eugene/units/PlusMinusOperatorsTest.java
@@ -3,8 +3,10 @@ package org.cidarlab.eugene.units;
 import static org.junit.Assert.*;
 
 import org.cidarlab.eugene.Eugene;
+import org.cidarlab.eugene.constants.EugeneConstants;
 import org.cidarlab.eugene.dom.Device;
 import org.cidarlab.eugene.dom.PartType;
+import org.cidarlab.eugene.dom.Variable;
 import org.cidarlab.eugene.dom.imp.container.EugeneCollection;
 import org.cidarlab.eugene.exception.EugeneException;
 import org.junit.Test;
@@ -145,6 +147,54 @@ public class PlusMinusOperatorsTest {
 		}
 	}
 
+	@Test
+	public void testDeploymentIssue() {
+		String script = "PartType PT; Device D; D = D + PT; println(D);";
+		try {
+			
+			new Eugene().executeScript(script);
+		
+		} catch(EugeneException ee) {
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testDesignTemplatePlusPartTypeInLoop() {
+		
+		String script = 
+				"PartType PT;" +
+				"Device D;" +
+				"num N = 5;" +
+				"for(num i=1; i<=N; i=i+1) { D = D + PT; }";
+		
+		try {
+			Eugene eug = new Eugene();
+			
+			EugeneCollection results = eug.executeScript(script);
+			
+			assertTrue(null != results);
+			assertTrue(results.contains("D"));
+			assertTrue(results.get("D") instanceof Device);
+			
+			Device D = (Device)results.get("D");
+			assertTrue(D.getComponents().size() == 5);
+			
+			assertTrue(null != results.get("N"));
+			assertTrue(results.get("N") instanceof Variable);
+			
+			Variable N = (Variable)results.get("N");
+			assertTrue(EugeneConstants.NUM.equals(N.getType()));
+			
+			for(int i=0; i<N.getNum(); i++) {			
+				assertTrue(D.getComponents().get(i).size() == 1);
+				assertTrue(D.getComponents().get(i).get(0) instanceof PartType);
+			}
+			
+		} catch(EugeneException ee) {
+			assertTrue(false);
+		}
+	}
 	
 	/*---------------
 	 * INVALID USES OF THE + OPERATOR
