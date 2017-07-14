385be0ed201d583dfaf087eee12136fbe00f394a
Shamseen <shamseen.c@gmail.com>
2017-07-14 19:10:22 -0400

    removed synbio stuff from logic layer

--- src/main/java/org/cidarlab/eugene/data/sbol/mapping/sbolConversionAPI/SBOLConversionLogicLayer.java
+++ src/main/java/org/cidarlab/eugene/data/sbol/mapping/sbolConversionAPI/SBOLConversionLogicLayer.java
@@ -13,21 +13,4 @@ import org.junit.Before;
  * @author Shamseen Rahman <shamseen at bu.edu>
  */
 public class SBOLConversionLogicLayer {
-
-    private SynBioHubAdaptor instance; //connection to symbiohub db
-
-    @Before
-    private void setUp() {
-        instance = new SynBioHubAdaptor("https://synbiohub.programmingbiology.org/");
-        try {
-            instance.login("shamseen@bu.edu", "cidarlab");
-        } catch (SynBioHubException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @After
-    private void tearDown() {
-        instance.logout();
-    }
 }
